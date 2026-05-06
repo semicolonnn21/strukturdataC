f770fdf1facf65035d402ec795d77370a700f354
dafarevanzajoanputra-art <dafarevanzajoanputra@gamil.com>
2026-05-06 10:03:03 +0700

    update

--- tugassssss/pertemuan11.py
+++ tugassssss/pertemuan11.py
@@ -1,9 +1,4 @@
-def penjumlahan_list(lst):
-    s = 0
-
-    for elemen in lst:
-        s += elemen
-
-    return s
-
-print(penjumlahan_list())
+data = {'a': 1,
+        'b': 2,
+        'c': 3}
+print(data['c'])
